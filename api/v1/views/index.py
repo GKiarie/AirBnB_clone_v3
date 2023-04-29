7f168a649407c341fb3ce8d5a8f01300b045f960
GKiarie <igitaukiarie@gmail.com>
2023-04-29 14:15:43 +0000

    updated resource status view

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """view for /status route"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def retrieve_objects():
+    """retrieve objects by type (Classes)"""
+    resource_dict = {}
+    classes = {"amenities": "Amenity", "cities": "City",
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
+    for key, value in classes.items():
+        resource_dict[key] = storage.count(value)
+    return jsonify(resource_dict)
