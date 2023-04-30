2ed7ebecb9281adcc3b0a78e9c153a185e3e9350
GKiarie <igitaukiarie@gmail.com>
2023-04-30 18:40:02 +0000

    updated users endpoint functionality

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -11,34 +11,39 @@ from flask import jsonify, request, abort
 @app_views.route("/users", methods=["GET", "POST"], strict_slashes=False)
 @app_views.route("/users/<user_id>",
                  methods=["GET", "DELETE", "PUT"], strict_slashes=False)
-def get_all_amenities(amenity_id=None):
-    amenity_objs = storage.all(Amenity).values()
-    obj_dicts = [obj.to_dict() for obj in amenity_objs]
-    if not amenity_id:
+def get_all_users(user_id=None):
+    user_objs = storage.all(User).values()
+    obj_dicts = [obj.to_dict() for obj in user_objs]
+    if not user_id:
         if request.method == "GET":
             return jsonify(obj_dicts), 200
         elif request.method == "POST":
             my_dict = request.get_json()
             if not my_dict:
                 abort(400, "Not a JSON")
-            if not my_dict.get('name'):
-                abort(400, "Missing name")
+            if not my_dict.get('email'):
+                abort(400, "Missing email")
+            if not my_dict.get('password'):
+                abort(400, "Missing password")
             else:
-                obj = Amenity(**my_dict)
+                obj = User(**my_dict)
                 obj.save()
                 return jsonify(obj.to_dict()), 201
     else:
-        obj = storage.get(Amenity, amenity_id)
+        obj = storage.get(User, user_id)
         if not obj:
             abort(404)
         if request.method == "GET":
-            return jsonify(storage.get(Amenity, amenity_id).to_dict()), 200
+            return jsonify(storage.get(User, user_id).to_dict()), 200
         elif request.method == "PUT":
             my_dict = request.get_json()
             if not my_dict:
                 abort(400, "Not a JSON")
             else:
-                obj = Amenity(**my_dict)
+                ignore_keys = ["id", "email", "created_at", "updated_at"]
+                my_dict = {key: value for key, value in my_dict.items() if key
+                           not in ignore_keys}
+                obj.__dict__.update(my_dict)
                 obj.save()
                 return jsonify(obj.to_dict()), 201
         elif request.method == "DELETE":
